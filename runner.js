ec9dbfe4e8742abc260994c33784a65d5ff7efab
maciejso <m.soczka@gmail.com>
2017-12-19 17:05:18 +0000

    update to newer version

--- runner.js
+++ runner.js
@@ -6,7 +6,7 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 code = fs.readFileSync('voting.sol').toString()
 compiledCode = solc.compile(code)
 abiDefinition = JSON.parse(compiledCode.contracts[':Voting'].interface)
-VotingContract = new Web3.eth.contract(abiDefinition)
+VotingContract = web3.eth.contract(abiDefinition)
 
 byteCode = compiledCode.contracts[':Voting'].bytecode
 deployedContract = VotingContract.new(['Rama','Nick','Jose'],{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
